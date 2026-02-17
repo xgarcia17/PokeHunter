a66bb935c367fd7ced5c2121eabb88e8ef6a7ff3
Xavier Garcia <xmangarcia17@gmail.com>
2026-02-17 15:39:01 -0800

    feat: modify the size of the components in the scanner

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,13 @@
 import NavBar from "@/components/navbar";
 import Scanner from "@/components/scanner";
 
-export default function ScannerPag() {
+export default function ScannerPage() {
   return (
     <div className="h-screen bg-gradient-to-br from-purple-100 via-blue-50 to-purple-50 flex flex-col overflow-hidden">
       {/* Navigation Bar */}
       <NavBar />
-      {/* Main Content */}
-      <div className="flex-1 overflow-auto max-w-7xl mx-auto w-full px-8 py-6">
+      {/* Main Content of Page : Scanner */}
+      <div className="flex-1 overflow-auto max-w-7xl mx-auto w-[80%] px-8 py-6">
         <Scanner />
       </div>
     </div>
